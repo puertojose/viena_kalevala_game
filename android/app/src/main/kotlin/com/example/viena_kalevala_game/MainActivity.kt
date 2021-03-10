bb5d25293db9ddbbc0a5f3ac0106d03ac2044551
Jose Luis Puerto Valencia <jlpuertov@gmail.com>
2021-03-10 16:26:18 +0100

    🏆chore(android embedding): flutter android v2 embedding fixed by creating a new flutter project and copying all relevant files 🤣

--- android/app/src/main/kotlin/com/example/viena_kalevala_game/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/viena_kalevala_game/MainActivity.kt
@@ -1,13 +1,6 @@
 package com.example.viena_kalevala_game
 
-import android.os.Bundle
-
-import io.flutter.app.FlutterActivity
-import io.flutter.plugins.GeneratedPluginRegistrant
+import io.flutter.embedding.android.FlutterActivity
 
 class MainActivity: FlutterActivity() {
-  override fun onCreate(savedInstanceState: Bundle?) {
-    super.onCreate(savedInstanceState)
-    GeneratedPluginRegistrant.registerWith(this)
-  }
 }
